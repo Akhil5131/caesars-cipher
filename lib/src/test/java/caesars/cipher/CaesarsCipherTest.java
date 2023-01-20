643e5eed5ada9c95393602522804a1fb4551da39
Akhil5131 <113959109+Akhil5131@users.noreply.github.com>
2023-01-20 00:28:10 -0500

    Intial Commit

---
+++ lib/src/test/java/caesars/cipher/CaesarsCipherTest.java
@@ -0,0 +1,19 @@
+package caesars.cipher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CaesarsCipherTest {
+	private CaesarsCipher caesarCipher = new CaesarsCipher();
+
+	@Test
+	void testCipheredMessagewithoffset12(){
+		assertEquals("Zai SdW kag Va[`Y faVSk",caesarCipher.cipher("how are you doing today", 12));
+}
+	@Test
+	void testEmptyString() {
+		assertEquals("",caesarCipher.cipher("",12));
+	}
+
+}
